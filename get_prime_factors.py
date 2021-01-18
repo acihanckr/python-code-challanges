34b5bc2161ac91670f3d8785901ff27261526bd3
acihanckr <acihan.cakar@gmail.com>
2021-01-17 22:38:24 -0500

    unneccessary is_prime function removed

--- get_prime_factors.py
+++ get_prime_factors.py
@@ -1,18 +1,10 @@
-def is_prime(n):
-    prime_stat = True
-    for i in range(2, n):
-        if i**2 <= n:
-            if n%i == 0:
-                prime_stat=False
-    return prime_stat
-
 def get_prime_factors(n):
-    if is_prime(n):
-        prime_factors = [n]
-    else:
-        for i in range(2, n):
-            if is_prime(i) and n%i == 0:
-                prime_factors = [i] + get_prime_factors(n//i)          
-    return sorted(prime_factors)
+    prime_factors = list()
+    for i in range(2, n+1):
+        if  n%i == 0 and i != n:
+            prime_factors = [i] + get_prime_factors(n//i)
+            return sorted(prime_factors) 
+        elif i == n:
+            return [i]         
 
 print(get_prime_factors(630))
