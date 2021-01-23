7baa897f3291f2ae83eddf339194eda418e21690
adnan <acihan.cakar@gmail.com>
2021-01-23 00:18:46 -0500

    index_all added

--- index_all.py
+++ index_all.py
@@ -1,15 +1,12 @@
+import numpy as np
 def index_all(lst, val):
-    
     indices = []
     for i, element in enumerate(lst):
         if element == val:
-            return i
-        elif isinstance(element,list):
-            index = index_all(element,val)
-            if isinstance(index, list):
+                indices.append([i])
+        elif isinstance(element, list):
+            for index in index_all(element,val):
                 indices.append([i] + index)
-            else:
-                indices.append([i,index])
     return indices
 
-print(index_all([[[1,2,3],2,[1,3]],[1,2,3]],2))
+print(index_all([[[1,2,2],2,[1,2]],[1,2,3]],2))
